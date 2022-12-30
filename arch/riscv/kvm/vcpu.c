3e35df320e684f039187101e0e4a0c62597138c2
Atish Patra <atishp@rivosinc.com>
2022-12-30 12:01:50 -0800

    RISC-V: KVM: Wireup TVM world switch
    TVM worlds switch takes a different path from regular VM world switch as
    it needs to make an ecall to TSM and TSM actually does the world switch.
    The host doesn't need to save/restore any context as TSM is expected
    to do that on behalf of the host. The TSM updatess the trap
    information in the shared memory which host uses to figure out the
    cause of the guest exit.
    
    Signed-off-by: Atish Patra <atishp@rivosinc.com>

--- arch/riscv/kvm/vcpu.c
+++ arch/riscv/kvm/vcpu.c
@@ -584,6 +584,11 @@ void kvm_arch_vcpu_put(struct kvm_vcpu *vcpu)
 	kvm_riscv_vcpu_host_vector_restore(&vcpu->arch.host_context);
 
 	if (kvm_riscv_nacl_available()) {
+		/**
+		 * For TVMs, we don't need a separate case as TSM only updates
+		 * the required CSRs during the world switch. All other CSR
+		 * value should be zeroed out by TSM anyways.
+		 */
 		nshmem = nacl_shmem();
 		csr->vsstatus = nacl_shmem_csr_read(nshmem, CSR_VSSTATUS);
 		csr->vsie = nacl_shmem_csr_read(nshmem, CSR_VSIE);
@@ -732,6 +737,12 @@ static void noinstr kvm_riscv_vcpu_enter_exit(struct kvm_vcpu *vcpu,
 		} else {
 			gcntx->hstatus = csr_swap(CSR_HSTATUS, hcntx->hstatus);
 		}
+
+		trap->htval = nacl_shmem_csr_read(nshmem, CSR_HTVAL);
+		trap->htinst = nacl_shmem_csr_read(nshmem, CSR_HTINST);
+	} else if (is_cove_vcpu(vcpu)) {
+		nshmem = nacl_shmem();
+		kvm_riscv_cove_vcpu_switchto(vcpu, trap);
 
 		trap->htval = nacl_shmem_csr_read(nshmem, CSR_HTVAL);
 		trap->htinst = nacl_shmem_csr_read(nshmem, CSR_HTINST);
