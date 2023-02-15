5499c0f2a48e7a4084a045fe1ed3ff20e7678513
Sunil V L <sunilvl@ventanamicro.com>
2023-02-15 22:44:05 +0530

    RISC-V: Add support to build the ACPI core
    Enable ACPI core for RISC-V after adding architecture-specific
    interfaces and header files required to build the ACPI core.
    
    1) Couple of header files are required unconditionally by the ACPI
    core. Add empty acenv.h and cpu.h header files.
    
    2) If CONFIG_PCI is enabled, a few PCI related interfaces need to
    be provided by the architecture. Define dummy interfaces for now
    so that build succeeds. Actual implementation will be added when
    PCI support is added for ACPI along with external interrupt
    controller support.
    
    3) A few globals and memory mapping related functions specific
    to the architecture need to be provided.
    
    Signed-off-by: Sunil V L <sunilvl@ventanamicro.com>
    Acked-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Reviewed-by: Andrew Jones <ajones@ventanamicro.com>

---
+++ arch/riscv/include/asm/acenv.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * RISC-V specific ACPICA environments and implementation
+ */
+
+#ifndef _ASM_ACENV_H
+#define _ASM_ACENV_H
+
+/* It is required unconditionally by ACPI core */
+
+#endif /* _ASM_ACENV_H */
---
+++ arch/riscv/include/asm/cpu.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#ifndef _ASM_CPU_H
+#define _ASM_CPU_H
+
+/* It is required unconditionally by ACPI core */
+
+#endif /* _ASM_CPU_H */
