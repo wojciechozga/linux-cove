ba9ee3d3ce599608dedbecc7c4f3ceea224545f4
Rob Bradford <rbradford@rivosinc.com>
2023-03-01 13:59:52 +0000

    virtio-net: Fix probe of virtio-net on kvmtool
    Since the following commit virtio-net on kvmtool has printed a warning
    during the probe:
    
    commit dbcf24d153884439dad30484a0e3f02350692e4c
    Author: Jason Wang <jasowang@redhat.com>
    Date:   Tue Aug 17 16:06:59 2021 +0800
    
        virtio-net: use NETIF_F_GRO_HW instead of NETIF_F_LRO
    
    [    1.865992] net eth0: Fail to set guest offload.
    [    1.872491] virtio_net virtio2 eth0: set_features() failed (-22); wanted 0x0000000000134829, left 0x0080000000134829
    
    This is because during the probing the underlying netdev device has
    identified that the netdev features on the device has changed and
    attempts to update the virtio-net offloads through the virtio-net
    control queue. kvmtool however does not have a control queue that supports
    offload changing (VIRTIO_NET_F_CTRL_GUEST_OFFLOADS is not advertised)
    
    The netdev features have changed due to validation checks in
    netdev_fix_features():
    
    if (!(features & NETIF_F_RXCSUM)) {
    	/* NETIF_F_GRO_HW implies doing RXCSUM since every packet
    	 * successfully merged by hardware must also have the
    	 * checksum verified by hardware.  If the user does not
    	 * want to enable RXCSUM, logically, we should disable GRO_HW.
    	 */
    	if (features & NETIF_F_GRO_HW) {
    		netdev_dbg(dev, "Dropping NETIF_F_GRO_HW since no RXCSUM feature.\n");
    		features &= ~NETIF_F_GRO_HW;
    	}
    }
    
    Since kvmtool does not advertise the VIRTIO_NET_F_GUEST_CSUM feature the
    NETIF_F_RXCSUM bit is not present and so the NETIF_F_GRO_HW bit is
    cleared. This results in the netdev features changing, which triggers
    the attempt to reprogram the virtio-net offloads which then fails.
    
    This commit prevents that set of netdev features from changing by
    preemptively applying the same validation and only setting
    NETIF_F_GRO_HW if NETIF_F_RXCSUM is set because the device supports both
    VIRTIO_NET_F_GUEST_CSUM and VIRTIO_NET_F_GUEST_TSO{4,6}
    
    Signed-off-by: Rob Bradford <rbradford@rivosinc.com>

--- drivers/net/virtio_net.c
+++ drivers/net/virtio_net.c
@@ -3932,11 +3932,13 @@ static int virtnet_probe(struct virtio_device *vdev)
 			dev->features |= dev->hw_features & NETIF_F_ALL_TSO;
 		/* (!csum && gso) case will be fixed by register_netdev() */
 	}
-	if (virtio_has_feature(vdev, VIRTIO_NET_F_GUEST_CSUM))
+	if (virtio_has_feature(vdev, VIRTIO_NET_F_GUEST_CSUM)) {
 		dev->features |= NETIF_F_RXCSUM;
-	if (virtio_has_feature(vdev, VIRTIO_NET_F_GUEST_TSO4) ||
-	    virtio_has_feature(vdev, VIRTIO_NET_F_GUEST_TSO6))
-		dev->features |= NETIF_F_GRO_HW;
+		/* This dependency is enforced by netdev_fix_features */
+		if (virtio_has_feature(vdev, VIRTIO_NET_F_GUEST_TSO4) ||
+		    virtio_has_feature(vdev, VIRTIO_NET_F_GUEST_TSO6))
+			dev->features |= NETIF_F_GRO_HW;
+	}
 	if (virtio_has_feature(vdev, VIRTIO_NET_F_CTRL_GUEST_OFFLOADS))
 		dev->hw_features |= NETIF_F_GRO_HW;
 
