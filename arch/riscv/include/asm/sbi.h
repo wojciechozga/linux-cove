68723d9ff2157156494d4897aac81b31c8408467
Wojciech Ozga <woz@zurich.ibm.com>
2024-06-07 04:30:15 -0500

    Enable CoVE guests to run in an environment that does not have AIA. Detect AIA presence by discovering that the TEE security monitor (TSM) supports AIA capability. If AIA is not present, inject external interrupts using the HVIP register when resuming execution of a virtual processor via the COVH tvm_vcpu_run() call.
    Signed-off-by: Wojciech Ozga <woz@zurich.ibm.com>

--- arch/riscv/include/asm/sbi.h
+++ arch/riscv/include/asm/sbi.h
@@ -411,8 +411,14 @@ struct sbi_cove_tsm_info {
 	/* Current state of the TSM */
 	enum sbi_cove_tsm_state tstate;
 
+	/* TSM implementation identifier */
+	uint32_t impl_id;
+
 	/* Version of the loaded TSM */
 	uint32_t version;
+
+	/* Capabilities of the TSM */
+	unsigned long capabilities;
 
 	/* Number of 4K pages required per TVM */
 	unsigned long tvm_pages_needed;
--- arch/riscv/kvm/cove.c
+++ arch/riscv/kvm/cove.c
@@ -150,6 +150,11 @@ __always_inline bool kvm_riscv_cove_enabled(void)
 	return riscv_cove_enabled;
 }
 
+__always_inline bool kvm_riscv_cove_capability(unsigned long cap)
+{
+	return tinfo.capabilities & BIT(cap);
+}
+
 static void kvm_cove_imsic_clone(void *info)
 {
 	int rc;
@@ -589,9 +594,9 @@ void noinstr kvm_riscv_cove_vcpu_switchto(struct kvm_vcpu *vcpu, struct kvm_cpu_
 
 	/*
 	 * Bind the vsfile here instead during the new vsfile allocation because
-	 * COVH bind call requires the TVM to be in finalized state.
+	 * COVI bind call requires the TVM to be in finalized state.
 	 */
-	if (tvcpuc->imsic.bind_required) {
+	if (kvm_riscv_cove_capability(KVM_COVE_TSM_CAP_AIA) && tvcpuc->imsic.bind_required) {
 		tvcpuc->imsic.bind_required = false;
 		rc = kvm_riscv_cove_vcpu_imsic_bind(vcpu, BIT(tvcpuc->imsic.vsfile_hgei));
 		if (rc) {
