92040df722f687b79cb649e1e32cc9100393de55
Wojciech Ozga <woz@zurich.ibm.com>
2024-06-07 04:02:09 -0500

    Support VM's promote_to_tvm() call. Request isolation from the hypervisor early in the kernel boot process only if the kernel was built with the CONFIG_RISCV_COVE_GUEST_PROMOTE command-line kernel parameter. CoVE guest images created in the multi-step creation process should not use this parameter.
    Signed-off-by: Wojciech Ozga <woz@zurich.ibm.com>

---
+++ arch/riscv/include/asm/cove_promote.h
@@ -0,0 +1,19 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Definitions for promotion of VM to TVM.
+ *
+ * Copyright (c) 2024 IBM
+ *
+ * Authors:
+ *     Wojciech Ozga <woz@zurich.ibm.com>
+ */
+
+#ifndef __RISCV_COVE_PROMOTE_H__
+#define __RISCV_COVE_PROMOTE_H__
+
+#ifdef CONFIG_RISCV_COVE_GUEST_PROMOTE
+#define COVE_PROMOTE_SBI_EXT_ID 0x434F5648
+#define COVE_PROMOTE_SBI_FID 0x15
+#endif /* CONFIG_RISCV_COVE_GUEST_PROMOTE */
+
+#endif /* __RISCV_COVE_PROMOTE_H__ */
