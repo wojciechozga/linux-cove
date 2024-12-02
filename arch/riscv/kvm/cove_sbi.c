83f36ee4aad79abf807d66d27f1002674513ab87
Wojciech Ozga <woz@zurich.ibm.com>
2024-12-02 05:31:57 -0600

    RISC-V: KVM: Define promote to TVM ABI
    Signed-off-by: Wojciech Ozga <woz@zurich.ibm.com>

--- arch/riscv/kvm/cove_sbi.c
+++ arch/riscv/kvm/cove_sbi.c
@@ -488,3 +488,18 @@ int sbi_covh_tvm_remove_pages(unsigned long tvmid,
 
 	return 0;
 }
+
+int sbi_covh_tsm_promote_to_tvm(unsigned long fdt_address,
+				unsigned long tap_addr,
+				unsigned long sepc,
+				unsigned long *tvmid)
+{
+	struct sbiret ret = sbi_ecall(SBI_EXT_COVH, SBI_EXT_COVH_PROMOTE_TO_TVM,
+				      fdt_address, tap_addr, sepc, 0, 0, 0);
+	if (ret.error) {
+		return sbi_err_map_linux_errno(ret.error);
+	}
+
+	*tvmid = ret.value;
+	return 0;
+}
