afc92e51cae9fc72544568948b41c29d5c0c49df
Wojciech Ozga <woz@zurich.ibm.com>
2024-12-02 02:49:07 -0600

    RISC-V: KVM: Support NACL for CoVE with HS mode
    Utilize the correct NACL features by testing whether a feature required
    for NACL exploitation in nested virtualized environments is present. If
    nested virtualization is not present, use the NACL setup_shared_memory()
    ABIi. Otherwise, use the entire NACL ABI.
    
    Signed-off-by: Wojciech Ozga <woz@zurich.ibm.com>

--- arch/riscv/kvm/vcpu.c
+++ arch/riscv/kvm/vcpu.c
@@ -532,7 +532,7 @@ void kvm_arch_vcpu_load(struct kvm_vcpu *vcpu, int cpu)
 		goto skip_load;
 	}
 
-	if (kvm_riscv_nacl_sync_csr_available()) {
+	if (unlikely(kvm_riscv_cove_enabled()) || kvm_riscv_nacl_sync_csr_available()) {
 		nshmem = nacl_shmem();
 		nacl_shmem_csr_write(nshmem, CSR_VSSTATUS, csr->vsstatus);
 		nacl_shmem_csr_write(nshmem, CSR_VSIE, csr->vsie);
@@ -603,7 +603,7 @@ void kvm_arch_vcpu_put(struct kvm_vcpu *vcpu)
 					 vcpu->arch.isa);
 	kvm_riscv_vcpu_host_vector_restore(&vcpu->arch.host_context);
 
-	if (kvm_riscv_nacl_available()) {
+	if (kvm_riscv_nacl_sync_csr_available()) {
 		/**
 		 * For TVMs, we don't need a separate case as TSM only updates
 		 * the required CSRs during the world switch. All other CSR
