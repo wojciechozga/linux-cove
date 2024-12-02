99ccc85583a1e79e2a4df713d978ddee0204d544
Wojciech Ozga <woz@zurich.ibm.com>
2024-12-02 06:48:32 -0600

    RISC-V: KVM: Allow VM to decide on TVM promotion
    Support the alternative way of creating TVM by letting the VM to decide
    when it should be promoted to TVM. The VM uses COVH promote_to_tvm()
    ABI at the time it wants to become TVM. Reflect this call and the VM
    architectural state to the TSM.
    
    Signed-off-by: Wojciech Ozga <woz@zurich.ibm.com>

--- arch/riscv/kvm/vcpu_sbi_covh.c
+++ arch/riscv/kvm/vcpu_sbi_covh.c
@@ -9,18 +9,79 @@
 #include <linux/err.h>
 #include <linux/kvm_host.h>
 #include <asm/sbi.h>
+#include <asm/kvm_cove.h>
+#include <asm/kvm_nacl.h>
+#include <asm/kvm_cove_sbi.h>
+
+static int kvm_riscv_cove_promote_to_tvm(struct kvm_vcpu *vcpu,
+					 unsigned long fdt_address,
+					 unsigned long tap_addr) {
+	struct kvm_cove_tvm_context *tvmc;
+	struct kvm_cpu_context *cntx;
+	struct kvm_vcpu *target_vcpu;
+	unsigned long target_vcpuid;
+	void *nshmem = nacl_shmem();
+	int rc, gpr_id, offset;
+
+	rc = kvm_riscv_cove_vm_single_step_init(vcpu->kvm);
+	if (rc)
+		goto exit;
+
+	tvmc = vcpu->kvm->arch.tvmc;
+	cntx = &vcpu->arch.guest_context;
+
+	kvm_for_each_vcpu(target_vcpuid, target_vcpu, vcpu->kvm) {
+		rc = kvm_riscv_cove_vcpu_init(target_vcpu);
+		if (rc)
+			goto deallocate_vcpus;
+		target_vcpu->requests = 0;
+	}
+
+	for (gpr_id = 1; gpr_id < 32; gpr_id++) {
+		offset = KVM_ARCH_GUEST_ZERO + gpr_id * sizeof(unsigned long);
+		nacl_shmem_gpr_write_cove(nshmem, offset,
+					  ((unsigned long *)cntx)[gpr_id]);
+	}
+	kvm_arch_vcpu_load(vcpu, smp_processor_id());
+	rc = sbi_covh_tsm_promote_to_tvm(fdt_address, tap_addr, cntx->sepc+4,
+					 &tvmc->tvm_guest_id);
+	if (rc)
+		goto deallocate_vcpus;
+
+	tvmc->finalized_done = true;
+	vcpu->kvm->arch.vm_type = KVM_VM_TYPE_RISCV_COVE;
+	kvm_info("CoVE Guest creation successful with guest id %lx\n", tvmc->tvm_guest_id);
+	return 0;
+
+deallocate_vcpus:
+	kvm_for_each_vcpu(target_vcpuid, target_vcpu, vcpu->kvm)
+		if (target_vcpu->arch.tc)
+			kfree(target_vcpu->arch.tc);
+	kfree(tvmc);
+
+exit:
+	return rc;
+}
+
 static int kvm_sbi_ext_covh_handler(struct kvm_vcpu *vcpu, struct kvm_run *run,
 				    struct kvm_vcpu_sbi_return *retdata)
 {
 	struct kvm_cpu_context *cp = &vcpu->arch.guest_context;
 	unsigned long funcid = cp->a6;
+        int ret;
+
 	switch (funcid) {
+	case SBI_EXT_COVH_PROMOTE_TO_TVM:
+		ret = kvm_riscv_cove_promote_to_tvm(vcpu, cp->a0, cp->a1);
+		return ret;
+
 	default:
 		kvm_err("%s: Unsupported guest SBI %ld.\n", __func__, funcid);
 		retdata->err_val = SBI_ERR_NOT_SUPPORTED;
 		return -EOPNOTSUPP;
 	}
 }
+
 const struct kvm_vcpu_sbi_extension vcpu_sbi_ext_covh = {
 	.extid_start = SBI_EXT_COVH,
 	.extid_end = SBI_EXT_COVH,
