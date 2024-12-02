f783cca47c2e4256cd008e4a06068b0a482943f1
Wojciech Ozga <woz@zurich.ibm.com>
2024-12-02 05:22:11 -0600

    RISC-V: KVM: Create CoVE guest in multiple steps
    Specialize the current TVM creation procedure as a multi-step TVM
    creation to differentiate from other ways of creating TVMs.
    
    Signed-off-by: Wojciech Ozga <woz@zurich.ibm.com>

--- arch/riscv/include/asm/kvm_cove.h
+++ arch/riscv/include/asm/kvm_cove.h
@@ -122,7 +122,7 @@ struct kvm_cove_tvm_context {
 
 static inline bool is_cove_vm(struct kvm *kvm)
 {
-	return kvm->arch.vm_type == KVM_VM_TYPE_RISCV_COVE;
+	return kvm->arch.vm_type == KVM_VM_TYPE_RISCV_COVE_MULTI_STEP_INIT;
 }
 
 static inline bool is_cove_vcpu(struct kvm_vcpu *vcpu)
@@ -133,6 +133,12 @@ static inline bool is_cove_vcpu(struct kvm_vcpu *vcpu)
 static inline bool is_cove_vm_initializing(struct kvm *kvm)
 {
 	return is_cove_vm(kvm) && !kvm->arch.tvmc->finalized_done;
+}
+
+static inline bool is_cove_vm_multi_step_initializing(struct kvm *kvm)
+{
+	return kvm->arch.vm_type == KVM_VM_TYPE_RISCV_COVE_MULTI_STEP_INIT && \
+	       !kvm->arch.tvmc->finalized_done;
 }
 
 static inline bool is_cove_vm_finalized(struct kvm *kvm)
@@ -148,7 +154,7 @@ int kvm_riscv_cove_init(void);
 
 /* TVM related functions */
 void kvm_riscv_cove_vm_destroy(struct kvm *kvm);
-int kvm_riscv_cove_vm_init(struct kvm *kvm);
+int kvm_riscv_cove_vm_multi_step_init(struct kvm *kvm);
 
 /* TVM VCPU related functions */
 void kvm_riscv_cove_vcpu_destroy(struct kvm_vcpu *vcpu);
@@ -183,7 +189,7 @@ static inline int kvm_riscv_cove_hardware_enable(void) {return 0; }
 
 /* TVM related functions */
 static inline void kvm_riscv_cove_vm_destroy(struct kvm *kvm) {}
-static inline int kvm_riscv_cove_vm_init(struct kvm *kvm) {return -1; }
+static inline int kvm_riscv_cove_vm_multi_step_init(struct kvm *kvm) { return -1; }
 
 /* TVM VCPU related functions */
 static inline void kvm_riscv_cove_vcpu_destroy(struct kvm_vcpu *vcpu) {}
